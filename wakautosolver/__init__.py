697fec53941860a103ac0e30c5ec8b6dc1a36d9c
Michael H <michael@michaelhall.tech>
2023-10-26 20:35:56 -0400

    wheels for pyodide?

---
+++ wakautosolver/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (C) 2023 Michael Hall <https://github.com/mikeshardmind>
+"""
+from .stdlib_standalone import Config as Config  # noqa: PLC0414
+from .stdlib_standalone import solve_config as solve_config  # noqa: PLC0414
+from .stdlib_standalone import v1_lv_class_solve as v1_lv_class_solve  # noqa: PLC0414
+
+__version__ = "2023.10.26"
